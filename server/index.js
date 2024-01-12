82b80eb0e6827361e3ad1dda8c290b6e5d6fcced
kirandream7777 <kirandream7777@gamil.com>
2024-01-12 23:25:31 +0530

    Init the project

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
