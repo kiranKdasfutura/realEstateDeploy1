8875dd3523eef964d4ac5f1838e28214e6138d19
kirandream7777 <kirandream7777@gamil.com>
2024-01-20 13:02:22 +0530

    compleet profile page UI

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -3,14 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 const PrivateRoute = () => {
   const { currentUser } = useSelector((state) => state.user);
-  return (
-    <div>
-        {
-            currentUser?<Outlet />:<Navigate to={'/sign-in'}/>
-        }
-      
-    </div>
-  );
+  return <div>{currentUser ? <Outlet /> : <Navigate to={"/sign-in"} />}</div>;
 };
 
 export default PrivateRoute;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
 const Profile = () => {
+  const {currentUser}=useSelector((state)=>state.user)
   return (
-    <div>Profile</div>
-  )
+    <>
+      <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+        <form className="flex flex-col gap-4">
+          <img
+            src={currentUser.avatar}
+            className="h-24 w-24 self-center rounded-full object-cover cursor-pointer mt-2 "
+            alt=""
+          />
+          <input
+            type="text"
+            className="p-3 rounded-lg border "
+            placeholder="username"
+            id="username"
+          />
+          <input
+            type="email"
+            className="p-3 rounded-lg border "
+            placeholder="email"
+            id="email"
+          />
+          <input
+            type="password"
+            className="p-3 rounded-lg border "
+            placeholder="password"
+            id="password"
+          />
+          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            update
+          </button>
+        </form>
+        <div className='flex justify-between mt-5'>
+          <span className="text-red-700 cursor-pointer">Delete accound</span>
+          <span className="text-red-700 cursor-pointer">Sign out</span>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Profile
