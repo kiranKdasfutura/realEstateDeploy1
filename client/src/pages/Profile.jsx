11e456315da037b8e4b06405b8f3cc7d60be805e
kirandream7777 <kirandream7777@gamil.com>
2024-01-29 00:41:12 +0530

    complete show user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -28,6 +28,8 @@ const Profile = () => {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [listingError, setListingError] = useState(false)
+  const [userListings, setUserListings] = useState([])
   const dispatch = useDispatch();
   console.log(formData);
 
@@ -125,6 +127,21 @@ const Profile = () => {
       console.log(error);
     }
   };
+  const handleShowListings=async()=>{
+    try {
+      setListingError(false)
+      const res=await axiosInstance.get(`/user/listings/${currentUser._id}`)
+      setUserListings(res.data)
+      console.log(res.data);
+    } catch (error) {
+       if (error.response.data.success === false) {
+         setListingError(true);
+         return;
+       }
+       
+      setListingError(true)
+    }
+  }
   return (
     <>
       <div className="p-3 max-w-lg mx-auto">
@@ -216,8 +233,44 @@ const Profile = () => {
           ""
         )}
       </div>
+      <button
+        type="button"
+        onClick={handleShowListings}
+        className="text-green-700 w-full"
+      >
+        Show Listings
+      </button>
+      <p>{listingError ? "Error on listing try to compromice " : ""}</p>
+      {userListings &&
+        userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listing</h1>
+        {userListings.map((listing) => (
+          <div
+            key={listing._id}
+            className="border gap-4 rounded-lg p-3 flex justify-between items-center"
+          >
+            <Link to={`/listing/${listing._id}`}>
+              <img
+                src={listing.imageUrls[0]}
+                alt="listing cover"
+                className="h-16 w-16 object-contain"
+              />
+            </Link>
+            <Link
+              className="flex-1 text-slate-700 font-semibold  hover:underline truncate"
+              to={`/listing/${listing._id}`}
+            >
+              <p className="">{listing.name} </p>
+            </Link>
+            <div className="flex flex-col items-center">
+              <button className="text-red-700 uppercase">delete</button>
+              <button className="text-green-700 uppercase">edit</button>
+            </div>
+          </div>
+        ))}
+        </div> }
     </>
   );
 };
-
 export default Profile;
