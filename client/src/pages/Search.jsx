a2c1e12b4781e2d1ae46f40ffc8f8cc25a22fa3b
kirandream7777 <kirandream7777@gamil.com>
2024-01-31 14:46:56 +0530

    home page completed

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+// import ListingItem from "../components/ListingItem";
 import ListingItem from "../components/ListingItem";
+
 import { axiosInstance } from "../axios/requestMethods";
 
 export default function Search() {
