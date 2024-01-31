3d9b075ecacf1d64cb36eb53ee535e34456aec27
kirandream7777 <kirandream7777@gamil.com>
2024-01-31 15:40:47 +0530

    add preinstallation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from 'path'
 //Routes
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
@@ -35,9 +36,19 @@ mongoose
 app.listen(3000, () => {
   console.log("server running on port 3000");
 });
+
+const __dirname=path.resolve()
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing",listingRouter)
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 //error handle middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
